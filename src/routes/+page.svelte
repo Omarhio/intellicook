b8b1420b630755558175be344819ef29c4a1d975
NabilF01 <nabilfarhat01@gmail.com>
2025-01-06 16:01:09 +0100

    feat: ajout de l'icone dans la navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,10 @@
 	<header class="bg-[#FFCAD4] p-4 shadow-md">
 		<nav class="mx-auto flex max-w-6xl items-center justify-between">
 			<!-- Logo -->
-			<div class="font-title text-3xl text-[white]">Intellicook 🍳</div>
+			<div class="flex items-center gap-2">
+				<img src="/logo.webp" alt="Intellicook Logo" class="h-10 w-auto" />
+				<span class="font-title text-3xl text-[#F4ACB7]">Intellicook</span>
+			</div>
 
 			<!-- Navigation Links -->
 			<ul class="flex gap-4 font-body text-lg">
