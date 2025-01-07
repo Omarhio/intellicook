13a69bf17d33c62515a1df211b1b649c404b4d5d
NabilF01 <nabilfarhat01@gmail.com>
2025-01-07 11:11:38 +0100

    fix: ajout des tailles d'écran pour le responsive dans le fichier tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,16 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				title: ['Pacifico', 'cursive'], // Police pour les titres
-				body: ['Noto Sans JP', 'sans-serif'], // Police pour le texte principal
+				title: ['Pacifico', 'cursive'],
+				body: ['Noto Sans JP', 'sans-serif'],
 			}
-		}
+		},
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+		},
 	},
 
 	plugins: [typography, forms, containerQueries]
