1492d6a3db279659cbc4d9aaf9d125ea994544ec
Omarhio <big-dz@hotmail.fr>
2025-01-08 16:40:05 +0100

    feat: amelioration de la notification

--- src/routes/favoris/+page.svelte
+++ src/routes/favoris/+page.svelte
@@ -22,41 +22,41 @@
 <main class="relative flex min-h-screen flex-col items-center bg-[#FFF6F6] p-8 text-[#9D8189]">
 	<h1 class="text-center text-4xl text-[#F4ACB7]">Vos Favoris ❤️</h1>
 
-	{#if showHint}
-		<div
-			class="animate-fade absolute top-16 flex items-center gap-4 rounded-lg bg-[#9D8189] px-6 py-3 text-white shadow-md"
-		>
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" class="h-8 w-8">
-				<path
-					fill="#fffaf0"
-					stroke="#5f363a"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="3"
-					d="M22.09,48.28a6.83,6.83,0,0,0-4.95-3.91A20.22,20.22,0,0,1,20.93,4.3a20.53,20.53,0,0,1,9.7,2.56,7.26,7.26,0,0,0,7.15-.16A16.8,16.8,0,0,1,60.36,30.54a6.89,6.89,0,0,0-1,5.45,20.52,20.52,0,0,1,.48,4.34A19.48,19.48,0,0,1,40.37,59.7C29,59.7,24.08,52.71,22.09,48.28Z"
-				></path>
-				<circle
-					cx="37.21"
-					cy="30"
-					r="10.63"
-					fill="#ffdd7d"
-					stroke="#5f363a"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="3"
-				></circle>
-			</svg>
-			Cliquez sur l'œuf pour retirer une recette de vos favoris !
-		</div>
-	{/if}
-
 	{#if favoris.length > 0}
 		<div class="mt-12 w-full max-w-6xl">
 			<ul class="grid grid-cols-1 justify-center gap-8 sm:grid-cols-2 lg:grid-cols-3">
-				{#each favoris as recette (recette.nom)}
+				{#each favoris as recette, index (recette.nom)}
 					<li
 						class="relative mx-auto max-w-sm rounded-lg border p-4 shadow-lg transition-all hover:bg-[#FDE2E4]"
 					>
+						{#if index === 0 && showHint}
+							<div
+								class="animate-fade absolute -top-12 left-1/2 flex -translate-x-1/2 items-center gap-4 rounded-lg bg-[#9D8189] px-6 py-3 text-white shadow-md"
+							>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" class="h-8 w-8">
+									<path
+										fill="#fffaf0"
+										stroke="#5f363a"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="3"
+										d="M22.09,48.28a6.83,6.83,0,0,0-4.95-3.91A20.22,20.22,0,0,1,20.93,4.3a20.53,20.53,0,0,1,9.7,2.56,7.26,7.26,0,0,0,7.15-.16A16.8,16.8,0,0,1,60.36,30.54a6.89,6.89,0,0,0-1,5.45,20.52,20.52,0,0,1,.48,4.34A19.48,19.48,0,0,1,40.37,59.7C29,59.7,24.08,52.71,22.09,48.28Z"
+									></path>
+									<circle
+										cx="37.21"
+										cy="30"
+										r="10.63"
+										fill="#ffdd7d"
+										stroke="#5f363a"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="3"
+									></circle>
+								</svg>
+								Cliquez sur l'œuf pour retirer une recette de vos favoris !
+							</div>
+						{/if}
+
 						<img
 							src={recette.image}
 							alt={recette.nom}
